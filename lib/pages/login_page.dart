dabf8f9adfd26870bc6cdd6463d2fdaae95b5a38
Matthew McGowan <mattmc1099@gmail.com>
2023-09-10 13:15:22 -0400

    Created a base project for us to build off of. Created a signup/signin screen which is functional with firebase. Created firebase functionality for logging in with an email and password

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,222 @@
+import 'package:atlas/components/square_tile.dart';
+import 'package:atlas/components/my_button.dart';
+import 'package:atlas/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //Text Editing Controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //Sign user in method
+  void signUserIn() async {
+
+      //Displays a loading circle
+      showDialog(
+        context: context,
+        builder: (context) {
+            return const Center (
+              child: CircularProgressIndicator()
+            );
+          }
+        );
+
+      //Atempts to sign the user in
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text
+      );
+      //Gets rid of the loading circle
+      Navigator.pop(context);
+      } on FirebaseAuthException catch (e) {
+      //Gets rid of the loading circle
+      Navigator.pop(context);
+        //If email is wrong
+        if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
+          wrongEmailMessage();
+        //If password is wrong
+        } else if (e.code == 'The email address is badly formatted') {
+          wrongPasswordMessage();
+        }
+      }
+    }
+
+      //Wrong email popup
+      void wrongEmailMessage() {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return const AlertDialog(
+              title: Text('Incorrect email or password'),
+            );
+          }
+        );
+      }
+
+    //Wrong password popup
+    void wrongPasswordMessage() {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text('Incorrect Password'),
+          );
+        }
+      );
+    }
+
+  //Login Page setup
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea (
+        child: Center(
+          child: Column (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //White space above logo
+              const SizedBox(height: 25),
+
+              //Logo
+              SizedBox (
+                height: 120,
+                width: 120,
+                //color: Colors.blue,
+                child: Image.asset(
+                  'lib/icons/fitness.png'
+                )
+              ),
+
+              const SizedBox(height: 5),
+
+               //Atlas title
+              Text(
+                'Atlas',
+                style: TextStyle(
+                  color: Colors.blue[700],
+                  fontSize: 32,
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              //Username textfield
+              MyTextField(
+                controller: emailController,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              //Password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              //Forgot Password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text (
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              //Sign-in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 15),
+
+              //Continue
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      'Or continue with',
+                      style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              //Apple and Google sign-in
+              const Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Google button
+                  SquareTile(imagePath: 'lib/images/google-logo-transparent.png'),
+
+                  SizedBox(width: 30),
+
+                  //Apple button
+                  SquareTile(imagePath: 'lib/images/apple-logo-transparent.png')
+                ],
+              ),
+
+              const SizedBox(height: 25),
+
+              //Register now
+              Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text('Not a member?',
+                style: TextStyle(color: Colors.grey[700]),
+                ),
+
+                const SizedBox(width: 4),
+
+                const Text('Register now',
+                  style: TextStyle(
+                    color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}

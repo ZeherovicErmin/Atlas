024474008cd900d5f8774d365edda1f60c705b98
daher3 <daher2324@gmail.com>
2023-09-17 01:42:03 -0400

    Update/test main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 //Atlas Fitness App CSC 4996
 import 'package:atlas/pages/fitness_page.dart';
 import 'package:atlas/pages/home_page.dart';
@@ -46,6 +47,31 @@ class _MyAppState extends State<MyApp> {
         '/fitpage': (context) => const FitPage(),
         '/home': (context) => const HomePage(),
       },
+    );
+  }
+}
+*/
+//Atlas Fitness App CSC 4996
+import 'package:flutter/material.dart';
+import 'pages/auth_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
     );
   }
 }

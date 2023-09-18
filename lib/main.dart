172bf671c4036520508bcbd7d08fa8d4e2b62075
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-09-18 19:36:53 -0400

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();

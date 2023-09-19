202e8bf75d8acffcc7450eb80f51def617265d4d
Ayesha Helal <ayeshahelal11@gmail.com>
2023-09-19 00:47:13 -0400

    Recipes Homepage - initial with appBar

---
+++ atlas/lib/recipes.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+
+class Recipes extends StatefulWidget {
+  const Recipes({super.key});
+
+  @override
+  _resultsState createState() => _resultsState();
+}
+
+class _resultsState extends State<Recipes> {
+
+  @override
+  Widget build(BuildContext conext) {
+    return Scaffold(appBar: appBar());
+  }
+
+  AppBar appBar() {
+    String userName = 'John Smith';
+    return AppBar(
+      title: Text('Welcome, $userName!',
+          style: const TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontSize: 18,
+              fontWeight: FontWeight.bold)),
+      backgroundColor: Color(0xffA9B7FF),
+      leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset('assets/icons/burger-menu.svg',
+              height: 20, width: 20),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(10))),
+    );
+  }
+}

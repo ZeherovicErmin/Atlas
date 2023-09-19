5e0d5a523e67688b1a1e1c09550cc437bff8ad57
Ayesha Helal <ayeshahelal11@gmail.com>
2023-09-19 00:47:13 -0400

    Recipes Homepage - initial with appBar

--- atlas/lib/main.dart
+++ atlas/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'recipes.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyHomePage(title: "Recipes"));
+        debugShowCheckedModeBanner: false, 
+        home: Recipes());
   }
 }
 
@@ -25,15 +27,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      )
-    );
+        appBar: AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text(widget.title),
+    ));
   }
 }

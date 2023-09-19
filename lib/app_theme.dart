fcf88d786412036a4f989922719dd941a4e67b4f
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-09-19 00:25:02 -0400

    Resetting to bug fix

--- lib/app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Dart File for all our app theming aspects
-
-class AppTheme {
-  static const TextStyle DefaultTextStyle = TextStyle(
-    fontFamily: 'Open Sans',
-  );
-}

7f9df4905f47c72d17bc116e3defbfa65f0ac06c
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-09-19 00:32:50 -0400

    Reimplemented P1

---
+++ lib/components/my_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap, required this.text});
+
+  //Adds button functionality
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 100),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

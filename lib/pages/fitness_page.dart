a2af1d1fdd557773adec79c30febd357ac3d86c6
daher3 <daher2324@gmail.com>
2023-09-20 18:44:12 -0400

    update profile page
    fixed the error in the profile page and username and bio can be edited now

--- lib/pages/fitness_page.dart
+++ lib/pages/fitness_page.dart
@@ -6,24 +6,72 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class FitPage extends StatefulWidget {
-  const FitPage({super.key});
+  const FitPage({Key? key});
 
   @override
   State<FitPage> createState() => _FitPageState();
 }
 
 // Creating tabs to navigate to other pages with navbar
-final tabs = [
-  const HomePage(),
-  const FitPage(),
+final tabs = const [
+  HomePage(),
+  FitPage(),
 ];
 
 class _FitPageState extends State<FitPage> {
   final currentUser = FirebaseAuth.instance.currentUser!;
+  final usersCollection = FirebaseFirestore.instance.collection("Users");
   int _currentIndex = 0;
 
-//edit field
-  Future<void> editField(String field) async {}
+  // Edit field
+  Future<void> editField(String field) async {
+    String newValue = "";
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          "Edit $field",
+          style: const TextStyle(color: Colors.blue),
+        ),
+        content: TextField(
+          autofocus: true,
+          style: TextStyle(color: Colors.white), // Change text color to white
+          decoration: InputDecoration(
+            hintText: "Enter new $field",
+            hintStyle: TextStyle(color: Colors.black),
+          ),
+          onChanged: (value) {
+            newValue = value;
+          },
+        ),
+        actions: [
+          // Cancel button
+          TextButton(
+            child: Text(
+              'Cancel',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+
+          // Save button
+          TextButton(
+            child: Text(
+              'Save',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            onPressed: () => Navigator.of(context).pop(newValue),
+          ),
+        ],
+      ),
+    );
+
+    // Update in Firestore
+    if (newValue.trim().length > 0) {
+      // Only update if there is something in the text field
+      await usersCollection.doc(currentUser.email).update({field: newValue});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,15 +87,24 @@ class _FitPageState extends State<FitPage> {
             .doc(currentUser.email)
             .snapshots(),
         builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          //get user data
-          if (snapshot.hasData && snapshot.data!.data() != null) {
-            final userData = snapshot.data!.data() as Map<String, dynamic>;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
 
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Center(
+              child: const Text('User data not found.'),
+            );
+          }
+
+          final userData = snapshot.data!.data() as Map<String, dynamic>?;
+
+          if (userData != null) {
             return ListView(
               children: [
                 const SizedBox(height: 50),
 
-                //profile pic
+                // Profile pic
                 const Icon(
                   Icons.fitness_center,
                   size: 72,
@@ -55,81 +112,89 @@ class _FitPageState extends State<FitPage> {
 
                 const SizedBox(height: 10),
 
-                //user email
-                Text(currentUser.email!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 38, 97, 185))),
+                // User email
+                Text(
+                  currentUser.email!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
 
                 const SizedBox(height: 50),
 
-                //user details
+                // User details
                 const Padding(
                   padding: EdgeInsets.only(left: 25.0),
                   child: Text(
                     'My Details',
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
                   ),
                 ),
 
-                //username
+                // Username
                 MyTextBox(
-                  text: userData['Username'],
-                  sectionName: 'Username',
-                  onPressed: () => editField('Username'),
+                  text: userData['username'],
+                  sectionName: 'username',
+                  onPressed: () => editField('username'),
                 ),
 
-                //bio
+                // Bio
                 MyTextBox(
-                  text: userData['Bio'],
-                  sectionName: 'Bio',
-                  onPressed: () => editField('Bio'),
+                  text: userData['bio'],
+                  sectionName: 'bio',
+                  onPressed: () => editField('bio'),
                 ),
 
                 const SizedBox(height: 50),
 
-                //user posts
+                // User posts
                 const Padding(
                   padding: EdgeInsets.only(left: 25.0),
                   child: Text(
                     'My Posts',
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
                   ),
                 ),
               ],
             );
-          } else if (snapshot.hasError) {
+          } else {
+            // Handle the case where userData is null
             return Center(
-              child: Text('Error${snapshot.error}'),
+              child: const Text('User data is null.'),
             );
           }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
         },
       ),
       drawer: myDrawer,
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: const Color.fromARGB(255, 38, 97, 185),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-            // Using Navigator to put a selected page onto the stack
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => tabs[index]),
-            );
-          }),
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromARGB(255, 38, 97, 185),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          // Using Navigator to put a selected page onto the stack
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => tabs[index]),
+          );
+        },
+      ),
     );
   }
 }

0d42c2ec81acdc4259f064faa829bdb8f756a0a3
Lordekai <alichowdhurya@gmail.com>
2023-09-20 06:45:43 -0400

    update: Added functionality to access barcode logs

--- lib/pages/BarCodeLookupPage/barcode_log_page.dart
+++ lib/pages/BarCodeLookupPage/barcode_log_page.dart
@@ -1,15 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class BarCodeLogPage extends StatefulWidget {
-  const BarCodeLogPage({super.key});
+class BarcodeLogPage extends StatefulWidget {
+  const BarcodeLogPage({super.key});
 
   @override
-  State<BarCodeLogPage> createState() => _BarCodeLogPageState();
+  State<BarcodeLogPage> createState() => _BarcodeLogPageState();
 }
 
-class _BarCodeLogPageState extends State<BarCodeLogPage> {
+class _BarcodeLogPageState extends State<BarcodeLogPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Barcode logs'),
+      ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('Barcode_Lookup').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          final logs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: logs?.length,
+            itemBuilder: (context, index) {
+              final log = logs[index].data() as Map<String, dynamic>;
+              return ListTile(
+                title: Text('Barcode: $log["Barcode"]'),
+                subtitle: Text('product Name: '),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }

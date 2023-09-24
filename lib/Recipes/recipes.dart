c33dbb96fbf2db67a81c9c775e6c1919625ef142
Ayesha Helal <ayeshahelal11@gmail.com>
2023-09-24 01:04:57 -0400

    put recipe files in a Recipes folder

--- lib/Recipes/recipes.dart
+++ lib/Recipes/recipes.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:atlas/recipe-details.dart';
+import 'package:atlas/Recipes/recipe-details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';

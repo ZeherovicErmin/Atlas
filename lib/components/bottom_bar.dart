39a71c26c99525fbc94d680fb51ead89a35b1301
Lordekai <alichowdhurya@gmail.com>
2023-10-01 01:54:46 -0400

    update: made navbarcurved

--- lib/components/bottom_bar.dart
+++ lib/components/bottom_bar.dart
@@ -1,12 +1,14 @@
 import 'package:atlas/pages/barcode_log_page.dart';
 import 'package:atlas/pages/barcode_lookup_page.dart';
 import 'package:atlas/pages/home_page.dart';
+import 'package:atlas/pages/home_page2.dart';
 import 'package:atlas/pages/recipes.dart';
 import 'package:atlas/pages/user_profile.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 var indexProvider = StateProvider((ref) => 0);
 
@@ -21,26 +23,47 @@ class BottomNav extends ConsumerWidget {
       BarcodeLookupPage(),
       Recipes(),
       const UserProfile(),
+      HomePage2(),
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Riverpod Navigation"),
-      ),
+      // Indexed Stack holds the index of the page
+      // So the programmer knows what page you are on
       body: IndexedStack(
         index: currentIndex,
+        // See 'pages' variable above
+        // Passes in pages of the application
         children: pages,
       ),
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: CurvedNavigationBar(
+        // Colors of the nav
+        backgroundColor: Color.fromARGB(255, 169, 183, 255),
+        color: const Color.fromARGB(255, 102, 20, 255),
+        // Defines animation duration
+        animationDuration: Duration(milliseconds: 300),
         onTap: (index) {
+          // Watches for any changes to the index provider
           ref.read(indexProvider.notifier).state = index;
           print(ref.read(indexProvider.notifier).state);
         },
-        currentIndex: ref.watch(indexProvider),
+        index: ref.watch(indexProvider),
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Page 2"),
-          BottomNavigationBarItem(icon: Icon(Icons.scanner), label: "Page 3"),
+          Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.scanner,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.scanner,
+            color: Colors.white,
+          ),
         ],
       ),
     );

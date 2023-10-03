f47290de1776dc0aeafa118a3758e90ec50400e5
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-10-03 00:35:25 -0400

    Created Fitness Center Tabs and Modified App bar Titles

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class HomePage extends ConsumerWidget {
         child: Scaffold(
           //Home page for when a user logs in
           backgroundColor: Colors.transparent,
-          appBar: myAppBar(context, ref, 'Home'),
+          appBar: myAppBar(context, ref, 'H o m e'),
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,

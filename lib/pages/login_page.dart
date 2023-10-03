5f14046971922df702fc358f39931f8475a3482e
Mattmcgoo <mattmc1099@gmail.com>
2023-10-02 23:28:23 -0400

    Added forgot_password page for users to reset their password

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,62 +11,51 @@ import 'package:atlas/pages/forgot_password_page.dart';
 class LoginPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-  // watching the provider in main.dart for user changes
-  final user = ref.watch(userProvider);
-  final emailController = ref.watch(emailControllerProvider);
-  final passwordController = ref.watch(passwordControllerProvider);
+    // watching the provider in main.dart for user changes
+    final user = ref.watch(userProvider);
+    final emailController = ref.watch(emailControllerProvider);
+    final passwordController = ref.watch(passwordControllerProvider);
 
-  final auth = FirebaseAuth.instance;
+    final auth = FirebaseAuth.instance;
 
-  void showErrorMessage(String message) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.red,
-          title: Center(
-            child: Text(
-              message,
-              style: const TextStyle(color: Colors.white),
+    void showErrorMessage(String message) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.red,
+            title: Center(
+              child: Text(
+                message,
+                style: const TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-        );
-      },
-    );
-  }
-
-  // Function to handle signing in to firebase
-  Future<void> signIn(BuildContextcontext) async {
-    final email = emailController.text.trim();
-    final password = passwordController.text.trim();
-
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
+          );
+        },
       );
-
-      // Successful login
-      Navigator.of(context).pushReplacementNamed('/start');
-    } catch (e) {
-      showErrorMessage('Email or password is incorrect');
-      print("Sign-in failed: $e");
     }
-  }
 
-  return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color.fromARGB(255, 90, 117, 255),
-            Color.fromARGB(255, 161, 195, 250),
-          ],
-        ),
-      ),
-    child: Scaffold(
-        backgroundColor: Colors.transparent,
+    // Function to handle signing in to firebase
+    Future<void> signIn(BuildContextcontext) async {
+      final email = emailController.text.trim();
+      final password = passwordController.text.trim();
+
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        // Successful login
+        Navigator.of(context).pushReplacementNamed('/start');
+      } catch (e) {
+        showErrorMessage('Email or password is incorrect');
+        print("Sign-in failed: $e");
+      }
+    }
+
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 169, 183, 255),
         body: SafeArea(
           child: Center(
             child: SingleChildScrollView(
@@ -113,66 +102,120 @@ class LoginPage extends ConsumerWidget {
                       obscureText: true,
                     ),
 
-                const SizedBox(height: 15),
+                    const SizedBox(height: 10),
 
-                //Forgot Password
-                Padding (
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector (
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) {
-                            return ForgotPasswordPage();
-                          }));
-                        },
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Color.fromARGB(255, 0, 60, 255),
-                          fontWeight: FontWeight.bold,
+                    //Forgot Password
+                    Padding (
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector (
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                                return ForgotPasswordPage();
+                              }));
+                            },
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Color.fromARGB(255, 0, 60, 255),
+                              fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    //Sign-in button
+                    MyButton(
+                      text: 'Sign In',
+                      onTap: () => signIn(context),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    /*
+                  NOT FUNCTIONAL YET
+                    //Continue
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.5,
+                              color: Colors.grey[400],
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Text(
+                              'Or continue with',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.5,
+                              color: Colors.grey[400],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    //Apple and Google sign-in
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        //Google button
+                        SquareTile(
+                            imagePath:
+                                'lib/images/google-logo-transparent.png'),
+
+                        SizedBox(width: 30),
+
+                        //Apple button
+                        SquareTile(
+                            imagePath: 'lib/images/apple-logo-transparent.png')
+                      ],
+                    ),
+
+                  */
+
+                    const SizedBox(height: 25),
+
+                    //Register now
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Not a member?',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        const SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushNamed('/register');
+                          },
+                          child: const Text(
+                            'Register now',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 0, 60, 255),
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-
-            const SizedBox(height: 15),
-
-            //Sign-in button
-            MyButton(
-              text: 'Sign In',
-              onTap: () => signIn(context),
-            ),
-
-            const SizedBox(height: 25),
-
-          //Register now
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Not a member?',
-                style: TextStyle(color: Colors.black),
-              ),
-              const SizedBox(width: 4),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushNamed('/register');
-                },
-                child: const Text(
-                  'Register now',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 60, 255),
-                      fontWeight: FontWeight.bold),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ]),
+                  ]),
+            ),
           ),
-        ),
-    )));
+        ));
   }
 }

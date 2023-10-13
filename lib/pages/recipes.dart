a6f45672957994a62d9ef691b0ab3ab8ed99657b
Ayesha Helal <ayeshahelal11@gmail.com>
2023-10-13 03:55:58 -0400

    Initial Saved Recipe functionality - Add, remove, output saved recipes

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -12,6 +12,11 @@ final resultProvider = StateProvider<RecipeModel>((ref) {
   return RecipeModel();
 });
 
+//State Provider holds list of saved recipes
+final savedRecipesProvider = StateProvider<List<Result>>((ref) {
+  return [];
+});
+
 //Recipe Class that handles and displays the recipes is
 //child of class Consumer Widget
 class Recipes extends ConsumerWidget {
@@ -26,18 +31,20 @@ class Recipes extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //recipe provider state getter
+    //recipes provider state getter
     final recipes = ref.watch(resultProvider).results;
+    //Saved recipes provider state getter
+    List<Result> savedRecipes = ref.watch(savedRecipesProvider);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: myAppBar2(context, ref, "Recipes"),
-      body: gradient(recipes, context, ref),
+      body: gradient(recipes, savedRecipes, context, ref),
       //Recipe search bar submit button
       floatingActionButton: FloatingActionButton(
         //Use onSubmit to activate search, onSubmitTEST to deactivate search
         //and use test data
-        onPressed: () => onSubmit(context, ref),
+        onPressed: () => onSubmitTEST(context, ref),
         child: Text('Submit'),
       ),
     );
@@ -45,7 +52,8 @@ class Recipes extends ConsumerWidget {
 
 
   // bg gradient color
-  Widget gradient(recipes, context, ref) {
+  Widget gradient(List<Result>? recipes, List<Result>? savedRecipes,
+      BuildContext context, WidgetRef ref) {
     return Container(
       //gradient decoration
       decoration: const BoxDecoration(
@@ -60,7 +68,15 @@ class Recipes extends ConsumerWidget {
       ),
       //Rest of page material including search form and list
       //recipes returned from API
-      child: Column(children: [form(), recipeList(recipes, context, ref)]),
+      child: Column(children: [
+        form(),
+        recipeList(recipes, context, ref),
+        const Text(
+          "SAVED RECIPES",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        savedRecipeList(savedRecipes, context, ref),
+      ]),
     );
   }
 
@@ -115,18 +131,20 @@ class Recipes extends ConsumerWidget {
 
   // List of recipe results from the API request
   Widget recipeList(
-      List<Result> recipes,
+      //List<dynamic> recipes,
+      List<Result>? recipes,
       BuildContext context,
       WidgetRef ref) {
     //Expanded takes up entire container space
     if (recipes == null) {
-      return Text("INVALID API RESPONSE");
+      return const Text("INVALID API RESPONSE");
     }
     return Expanded(
         child: Padding(
             padding: const EdgeInsets.all(8.0),
             //ListView used to output recipe list element into individual components
             child: ListView.separated(
+              shrinkWrap: true,
               //Used to ensure list is scrollable
               physics: const AlwaysScrollableScrollPhysics(),
               //Number of recipes
@@ -140,13 +158,31 @@ class Recipes extends ConsumerWidget {
                         textAlign: TextAlign.center,
                         style: const TextStyle(fontWeight: FontWeight.bold)),
                     //Function used to capture tap event for list items
-                    onTap: () => navigateToRecipeDetails(context, recipe));
+                    onTap: () => navigateToRecipeDetails(context, recipe),
+                    //Add Save button to end of tile
+                    trailing: ElevatedButton(
+                        child: const Text("Save"),
+                        onPressed: () => onSave(recipe, ref, context)));
               },
               //Used to put a divider line between recipes
               separatorBuilder: (context, index) {
                 return const Divider();
               },
             )));
+  }
+
+  //Save Button Handler - Save New Recipe
+  void onSave(Result recipe, WidgetRef ref, BuildContext context) async {
+    //copy list of saved recipes
+    List<Result> recipes = [...ref.watch(savedRecipesProvider)];
+    //add new recicpe
+    recipes.add(recipe);
+    //save new list of recipes
+    ref.read(savedRecipesProvider.notifier).state = recipes;
+    //output recipe saved message
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Recipe Saved'), duration: Duration(seconds: 1)));
+    print(ref.watch(savedRecipesProvider));
   }
 
   //Form Submission Handler - Submits recipe search to API
@@ -216,5 +252,63 @@ class Recipes extends ConsumerWidget {
         builder: (context) => RecipeDetails(recipe: recipe),
       ),
     );
+  }
+
+  Widget savedRecipeList(
+      //List<dynamic> recipes,
+      List<Result>? recipes,
+      BuildContext context,
+      WidgetRef ref) {
+    if (recipes != null) {
+      return Expanded(
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              //ListView used to output recipe list element into individual components
+              child: ListView.separated(
+                shrinkWrap: true,
+                //Used to ensure list is scrollable
+                physics: const AlwaysScrollableScrollPhysics(),
+                //Number of recipes
+                itemCount: recipes.length,
+                //Used to build recipe list tiles
+                itemBuilder: (context, index) {
+                  Result recipe = recipes[index];
+                  String recipeName = recipe.title;
+                  return ListTile(
+                    onTap: () => navigateToRecipeDetails(context, recipe),
+                    title: Text(recipeName,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                    //Add remove button to end of tile
+                    trailing: ElevatedButton(
+                        child: const Text("Remove"),
+                        onPressed: () => onRemove(recipe, ref, context),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.red)
+                          )
+                        )
+                      );
+              },
+                //Used to put a divider line between recipes
+                separatorBuilder: (context, index) {
+                  return const Divider();
+                },
+              )));
+    }
+    return const Text("No Saved Recipes");
+  }
+
+  //Remove Button Handler - Remove Saved Recipe
+  onRemove(Result recipe, WidgetRef ref, BuildContext context) {
+    //copy list of saved recipes
+    List<Result> recipes = [...ref.watch(savedRecipesProvider)];
+    //remove the selected recipe
+    recipes.remove(recipe);
+    //save new list without the removed recipe
+    ref.read(savedRecipesProvider.notifier).state = recipes;
+    //Output removed message
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Recipe Removed'), duration: Duration(seconds: 1)));
+    //print(ref.watch(savedRecipesProvider));
   }
 }

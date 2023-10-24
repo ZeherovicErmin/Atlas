291cf68315d5a17d38f94f5c33147ee4b97280a2
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-10-24 00:10:35 -0400

    Changing the code to be cleaner

--- lib/pages/fitness_center.dart
+++ lib/pages/fitness_center.dart
@@ -34,8 +34,12 @@ String capitalizeFirstLetter(String text) {
 }
 
 // Creating A map of Icons for each specific muscles
-final Map<String, IconData> muscleIcons = {
-  "abdominals": Icons.star,
+final Map<String, Widget> muscleIcons = {
+  "biceps": Image.asset(
+    'lib/images/bicepicon.png',
+    height: 50,
+    width: 50,
+  ),
 };
 
 // Creating A map of icons for the exercise type i.e strength or cardio
@@ -131,211 +135,7 @@ class FitCenter extends ConsumerWidget {
               // The Discover Tab Of the workouts page
 
               // Listing each muscle that will dynamically show a list of exercises for the clicked workout on a different page
-              ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  final muscle = list[index];
-                  final muscleColor = muscleColors[muscle];
-                  final icon = muscleIcons[
-                      muscle]; // Initalize each entry of the list to the muscle
-                  return InkWell(
-                    onTap: () async {
-                      final exercisesData = await getExercises(muscle);
-
-                      if (exercisesData.isNotEmpty) {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => Scaffold(
-                            appBar: AppBar(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 29, 74, 222),
-                              title: Text("Workouts for $muscle"),
-                            ),
-                            body: ListView.builder(
-                              itemCount: exercisesData.length,
-                              itemBuilder: (context, index) {
-                                final exercise = exercisesData[index];
-                                final exerciseType = exercise['type'];
-
-                                // Finding the icon for each exercise type
-                                final exerciseTypeIcon =
-                                    exerciseTypeIcons[exerciseType];
-
-                                return Container(
-                                  margin: const EdgeInsets.all(16.0),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(
-                                        12.0), // Add a border radius
-                                    border: Border.all(
-                                      width: .5,
-                                      style: BorderStyle.solid,
-                                      color: Colors.transparent,
-                                      // Set the border color and width
-                                    ),
-                                  ),
-                                  child: FlipCard(
-                                    fill: Fill.fillBack,
-                                    direction: FlipDirection.VERTICAL,
-                                    speed: 400,
-                                    front: Card(
-                                      elevation: 4.0,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                      ),
-                                      child: SizedBox(
-                                        width: double.infinity,
-                                        height: 150.0,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              exercise['name'],
-                                              textAlign: TextAlign.center,
-                                              style: const TextStyle(
-                                                fontSize: 32,
-                                              ),
-                                            ),
-                                            const SizedBox(height: 8.0),
-                                            Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.spaceEvenly,
-                                              children: [
-                                                Text(
-                                                  exercise['type'],
-                                                  style: const TextStyle(
-                                                    color: Colors.red,
-                                                    fontSize: 18,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  exercise['muscle'],
-                                                  style: const TextStyle(
-                                                    color: Colors.blue,
-                                                    fontSize: 18,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  exercise['equipment'],
-                                                  style: const TextStyle(
-                                                    color: Colors.green,
-                                                    fontSize: 18,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  exercise['difficulty'],
-                                                  style: const TextStyle(
-                                                    color: Colors.purple,
-                                                    fontSize: 18,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                            Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.spaceEvenly,
-                                              children: [
-                                                Icon(
-                                                  exerciseTypeIcon ??
-                                                      Icons.category,
-                                                  color: Colors.red,
-                                                  size: 18,
-                                                ),
-                                                Icon(
-                                                  exerciseTypeIcon ??
-                                                      Icons.category,
-                                                  color: Colors.blue,
-                                                  size: 18,
-                                                ),
-                                                Icon(
-                                                  exerciseTypeIcon ??
-                                                      Icons.category,
-                                                  color: Colors.green,
-                                                  size: 18,
-                                                ),
-                                                Icon(
-                                                  exerciseTypeIcon ??
-                                                      Icons.category,
-                                                  color: Colors.purple,
-                                                  size: 18,
-                                                ),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    back: Card(
-                                      child: SingleChildScrollView(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(16.0),
-                                          child: Text(
-                                            exercise['instructions'],
-                                            style:
-                                                const TextStyle(fontSize: 18),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ));
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                                "No Workout Data is Available for $muscle."),
-                          ),
-                        );
-                      }
-                    },
-
-                    // Styling elements for each specific muscle
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: muscleColor,
-                        borderRadius: BorderRadius.circular(24.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 4,
-                            offset: Offset(0, 2),
-                          ),
-                        ],
-                      ),
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 8.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            // Adding an icon to each specific muscle
-                            Icon(icon ?? Icons.fitness_center,
-                                size:
-                                    34), // Setting the default Icon if one does not exist
-                            Text(
-                              capitalizeFirstLetter(
-                                  muscle.replaceAll('_', ' ')),
-                              style: const TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const Icon(Icons.arrow_forward_ios, size: 34),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+              musclesList(),
 
               Container(
                 color: const Color.fromARGB(255, 232, 229, 229),
@@ -350,6 +150,206 @@ class FitCenter extends ConsumerWidget {
           ),
         ),
       ),
+    );
+  }
+
+  ListView musclesList() {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        final muscle = list[index];
+        final muscleColor = muscleColors[muscle];
+        final icon = muscleIcons[
+            muscle]; // Initalize each entry of the list to the muscle
+        return GestureDetector(
+          onTap: () async {
+            final exercisesData = await getExercises(muscle);
+
+            if (exercisesData.isNotEmpty) {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => Scaffold(
+                  appBar: AppBar(
+                    backgroundColor:
+
+                        //Workouts for each muscle group
+                        const Color.fromARGB(255, 29, 74, 222),
+                    title: Text("Workouts for $muscle"),
+                  ),
+                  body: exercisesList(exercisesData),
+                ),
+              ));
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("No Workout Data is Available for $muscle."),
+                ),
+              );
+            }
+          },
+
+          // Styling elements for each specific muscle
+          child: Container(
+            decoration: BoxDecoration(
+              color: muscleColor,
+              borderRadius: BorderRadius.circular(24.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Adding an icon to each specific muscle
+                  muscleIcons[muscle] ??
+                      Icon(Icons.fitness_center,
+                          size:
+                              34), // Setting the default Icon if one does not exist
+                  Text(
+                    capitalizeFirstLetter(muscle.replaceAll('_', ' ')),
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Icon(Icons.arrow_forward_ios, size: 34),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  ListView exercisesList(List<dynamic> exercisesData) {
+    return ListView.builder(
+      itemCount: exercisesData.length,
+      itemBuilder: (context, index) {
+        final exercise = exercisesData[index];
+        final exerciseType = exercise['type'];
+
+        // Finding the icon for each exercise type
+        final exerciseTypeIcon = exerciseTypeIcons[exerciseType];
+
+        return Container(
+          margin: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0), // Add a border radius
+            border: Border.all(
+              width: .5,
+              style: BorderStyle.solid,
+              color: Colors.transparent,
+              // Set the border color and width
+            ),
+          ),
+          child: FlipCard(
+            fill: Fill.fillBack,
+            direction: FlipDirection.VERTICAL,
+            speed: 400,
+            front: Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                height: 150.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      exercise['name'],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          exercise['type'],
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          exercise['muscle'],
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          exercise['equipment'],
+                          style: const TextStyle(
+                            color: Colors.green,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          exercise['difficulty'],
+                          style: const TextStyle(
+                            color: Colors.purple,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          exerciseTypeIcon ?? Icons.category,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                        Icon(
+                          exerciseTypeIcon ?? Icons.category,
+                          color: Colors.blue,
+                          size: 18,
+                        ),
+                        Icon(
+                          exerciseTypeIcon ?? Icons.category,
+                          color: Colors.green,
+                          size: 18,
+                        ),
+                        Icon(
+                          exerciseTypeIcon ?? Icons.category,
+                          color: Colors.purple,
+                          size: 18,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            back: Card(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    exercise['instructions'],
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }

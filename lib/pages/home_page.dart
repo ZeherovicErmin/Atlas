f4d377d05863aae856eea1965017792d1bd57724
Mattmcgoo <mattmc1099@gmail.com>
2023-10-26 04:31:09 -0400

    Updated the home page habit trackers to be able to store habit data per user per each day.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,117 +24,133 @@ class HabitCard extends StatefulWidget {
 
 class HabitCardState extends State<HabitCard> {
   late String currentSubtitle;
+  late Stream<DocumentSnapshot> habitStream;
 
   @override
   void initState() {
     super.initState();
     currentSubtitle = '';
-    fetchData();
+    habitStream = fetchData();
   }
 
-  //Gets the data back from firebase
-  void fetchData() async {
+  //Fetches the habit cards from firebase to display to the user
+  Stream<DocumentSnapshot> fetchData() {
     var currentDate = DateTime.now();
-    var formattedDate = "${currentDate.month}/${currentDate.day}/${currentDate.year}";
+    var formattedDate = "${currentDate.year}-${currentDate.month}-${currentDate.day}";
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User? user = auth.currentUser;
     final uid = user?.uid;
-    var data = await FirebaseFirestore.instance
-        .collection('Habits')
-        .doc(uid)
-        .get();
-    if (data.exists) {
-      setState(() {
-        currentSubtitle = data.data()?[widget.title] ?? '';
-      });
-    }
+    //For debugging purposes, change the testdate to any date to test the
+    //fetching of the correct data for the correct date.
+    var testDate = '2023-10-25';
+    return FirebaseFirestore.instance.collection('Habits').doc(uid).collection(formattedDate).doc('habits').snapshots();
   }
 
   //Opens the habit card for editing & habit card build
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        editDialog(context, widget.title);
+    return StreamBuilder<DocumentSnapshot>(
+      stream: habitStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        var data = snapshot.data?.data() as Map<String, dynamic>?;
+        currentSubtitle = data?[widget.title.toLowerCase()] ?? '';
+        return GestureDetector(
+          onTap: () {
+            editDialog(context, widget.title);
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.backgroundColor,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.iconData,
+                  size: 50,
+                  color: Colors.white,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  currentSubtitle,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: widget.backgroundColor,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              widget.iconData,
-              size: 50,
-              color: Colors.white,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              widget.title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              currentSubtitle,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
-
   //Function for editing a habit card
   //Sends the data to firebase
-  void editDialog(BuildContext context, String title) {
+  void editDialog(BuildContext context, String title) async {
+    //Variables
+    var currentDate = DateTime.now();
     TextEditingController textController = TextEditingController(text: currentSubtitle);
+    String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
+    var formattedDate = "${currentDate.year}-${currentDate.month}-${currentDate.day}";
+    //Habit card popup for editing data
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: Text('Edit $title'),
-          content: TextField(
-            controller: textController,
-            decoration: const InputDecoration(hintText: 'Enter a value'),
-            onChanged: (value) {
-              currentSubtitle = value;
-            },
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Save'),
-              onPressed: () async {
-                widget.onTap(currentSubtitle);
-                String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
-                DocumentReference docRef = FirebaseFirestore.instance
-                    .collection('Habits')
-                    .doc(uid);
-                await docRef.set(
-                  {title: currentSubtitle, 'uid': uid},
-                  SetOptions(merge: true),
-                );
-                Navigator.of(context).pop();
-                setState(() {
-                  currentSubtitle = textController.text;
-                });
-              },
-            ),
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Edit $title'),
+              content: TextField(
+                controller: textController,
+                decoration: const InputDecoration(hintText: 'Enter a value'),
+                onChanged: (value) {
+                  setState(() {
+                    currentSubtitle = value;
+                  });
+                },
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Save'),
+                  onPressed: () async {
+                    widget.onTap(currentSubtitle);
+                    DocumentReference userDocRef = FirebaseFirestore.instance.collection('Habits').doc(uid);
+                    CollectionReference dateSubcollectionRef = userDocRef.collection(formattedDate);
+                    await dateSubcollectionRef.doc('habits').set({
+                      title.toLowerCase(): currentSubtitle,
+                    }, SetOptions(merge: true));
+                    Navigator.of(context).pop();
+                    setState(() {
+                      currentSubtitle = textController.text;
+                    });
+                  },
+                ),
+                TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
         );
       },
     );
@@ -153,7 +169,6 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 0, 136, 204),
           actions: [
-          //Settings icon button
           IconButton (
           icon: const Icon(Icons.person_rounded),
           onPressed: () {
@@ -172,89 +187,103 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
+@override
+Widget build(BuildContext context) {
+  //Variables
+  var currentDate = DateTime.now();
+  var formattedDate = "${currentDate.month}/${currentDate.day}";
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User? user = auth.currentUser;
+  final uid = user?.uid;
+  String uid2 = uid.toString();
 
-    //Variables
-    var currentDate = DateTime.now();
-    var formattedDate = "${currentDate.month}/${currentDate.day}";
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    final User? user = auth.currentUser;
-    final uid = user?.uid;
-    String uid2 = uid.toString();
-
-    //Returns the app bar & habit cards
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: homePageAppBar(context, 'H o m e  P a g e'),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14),
-                  color: const Color.fromARGB(255, 255, 149, 1),
-                ),
-                child: TabBar(
-                  unselectedLabelColor: const Color.fromARGB(255, 255, 149, 1),
-                  indicator: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                  color: const Color.fromARGB(255, 255, 149, 1),
-                  ),
-                  tabs: [
-                    Tab(text: formattedDate),
-                  ],
-                ),
+  //Returns the app bar & habit cards
+  return DefaultTabController(
+    length: 3,
+    child: Scaffold(
+      appBar: homePageAppBar(context, uid2),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                color: const Color.fromARGB(255, 255, 149, 1),
               ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 20,
-                  children: [
-                    HabitCard(
-                      title: 'Calories',
-                      iconData: Icons.fastfood,
-                      backgroundColor: Colors.red,
-                      onTap: (value) {
-                      },
-                      selectedDate: formattedDate,
-                    ),
-                    HabitCard(
-                      title: 'Sleep',
-                      iconData: Icons.nightlight_round,
-                      backgroundColor: Colors.purple,
-                      onTap: (value) {
-                      },
-                      selectedDate: formattedDate,
-                    ),
-                    HabitCard(
-                      title: 'Water',
-                      iconData: Icons.water_drop,
-                      backgroundColor: Colors.blue,
-                      onTap: (value) {
-                      },
-                      selectedDate: formattedDate,
-                    ),
-                    HabitCard(
-                      title: 'Running',
-                      iconData: Icons.directions_run,
-                      backgroundColor: Colors.green,
-                      onTap: (value) {
-                      },
-                      selectedDate: formattedDate,
-                    ),
-                  ],
+              child: TabBar(
+                unselectedLabelColor: const Color.fromARGB(255, 255, 149, 1),
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color.fromARGB(255, 255, 149, 1),
                 ),
-              )
-            ],
-          ),
+                tabs: [
+                  Tab(text: formattedDate),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                children: [
+                  HabitCard(
+                    title: 'Calories',
+                    iconData: Icons.fastfood,
+                    backgroundColor: Colors.red,
+                    onTap: (value) {
+                      saveHabitData(uid2, formattedDate, 'calories', value);
+                    },
+                    selectedDate: formattedDate,
+                  ),
+                  HabitCard(
+                    title: 'Sleep',
+                    iconData: Icons.nightlight_round,
+                    backgroundColor: Colors.purple,
+                    onTap: (value) {
+                      saveHabitData(uid2, formattedDate, 'sleep', value);
+                    },
+                    selectedDate: formattedDate,
+                  ),
+                  HabitCard(
+                    title: 'Water',
+                    iconData: Icons.water_drop,
+                    backgroundColor: Colors.blue,
+                    onTap: (value) {
+                      saveHabitData(uid2, formattedDate, 'water', value);
+                    },
+                    selectedDate: formattedDate,
+                  ),
+                  HabitCard(
+                    title: 'Running',
+                    iconData: Icons.directions_run,
+                    backgroundColor: Colors.green,
+                    onTap: (value) {
+                      saveHabitData(uid2, formattedDate, 'running', value);
+                    },
+                    selectedDate: formattedDate,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
-    );
+    ),
+  );
+}
+
+  //Function to save habit data in Firebase
+  void saveHabitData(String uid, String formattedDate, String habit, String value) async {
+    CollectionReference habitCollectionRef = FirebaseFirestore.instance
+        .collection('Habits')
+        .doc(uid)
+        .collection(formattedDate)
+        .doc('habits')
+        .collection('habits');
+    await habitCollectionRef.doc(habit).set({'data': value});
   }
 }

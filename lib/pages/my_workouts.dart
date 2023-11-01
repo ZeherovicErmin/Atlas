d9b158c1b8073f6c44bca060b640fcba75b2e21d
ZeherovicErmin <ZeherovicErmin@gmail.com>
2023-11-01 13:19:40 -0400

    Importing saved_workouts.dart

--- lib/pages/my_workouts.dart
+++ lib/pages/my_workouts.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'saved_workouts.dart';
 
 // The My Workouts tab which will hold users saved exercises in a digestible workout format
 

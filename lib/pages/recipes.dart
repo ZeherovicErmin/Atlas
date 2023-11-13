ba7b082c5c04b9311034f1e4e56cafd6561b3175
Ayesha Helal <ayeshahelal11@gmail.com>
2023-11-13 02:22:19 -0500

    Recipe Search Filters Feature and Search Form Redesign: Added dietary restriction filters allowing the user to filter their search based on their dietary requirements. Also redeveloped recipe search form using FormBuilder library, and redesigned the form appearance using expandable tiles to allow more space for search result viewing.

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:atlas/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:atlas/pages/recipe-details.dart';
@@ -27,17 +28,14 @@ final calorieRangesProvider = StateProvider<RangeValues>((ref) {
   return const RangeValues(0, 2000);
 });
 
+//global key used for recipe form-handling
+final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
 //Recipe Class that handles and displays the recipes is
 //child of class Consumer Widget
 class Recipes extends ConsumerWidget {
   //Constructors
   Recipes({Key? key}) : super(key: key);
-
-  //global key used for recipe form-handling
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  //Text controller used to store value from recipe search bar
-  final TextEditingController searchController = TextEditingController();
 
   get themeColor => null;
 
@@ -69,11 +67,26 @@ class Recipes extends ConsumerWidget {
           body: TabBarView(children: [
             Column(children: [
               Padding(padding: EdgeInsets.only(top: 20)),
-              form(context, ref),
-              ingredientsList(context, ref),
+              SingleChildScrollView(
+              physics: ClampingScrollPhysics(),
+              child: ExpansionTile(
+                title: const Text(
+                  "Search",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  form(context, ref),
+                  Text("Ingredient Quick-Search", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  ingredientsList(context, ref),
+                ],
+                backgroundColor: Color.fromARGB(255, 248, 237, 220),
+                collapsedBackgroundColor: Color.fromARGB(255, 248, 237, 220),
+                initiallyExpanded: true,
+              )),
               recipeList(recipes, context, ref)
             ]),
-            const SavedRecipes(),
+            SavedRecipes(),
             CustomRecipes()
           ]),
         ));
@@ -81,10 +94,8 @@ class Recipes extends ConsumerWidget {
 
   //Recipe search form
   Widget form(BuildContext context, WidgetRef ref) {
-    RangeValues calorieRange = ref.watch(calorieRangesProvider);
     return Column(children: [
       //Spacing between components
-
       const Padding(
         padding: EdgeInsets.only(
             top: 5,
@@ -93,53 +104,38 @@ class Recipes extends ConsumerWidget {
             bottom: 5), //apply padding to all sides
       ),
       Container(
-        margin: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.11))
-          ],
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(children: [
-            searchBar(context, ref),
-            Container(
-                color: Color(0xFFFAF9F6),
-                padding: EdgeInsets.only(top: 15),
-                alignment: Alignment.topLeft,
-                child: Text("Calorie Filter",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontWeight: FontWeight.bold,
-                    ))),
-            Container(
-                color: Color(0xFFFAF9F6),
-                child: RangeSlider(
-                    values: calorieRange,
-                    max: 5000,
-                    divisions: 50,
-                    activeColor: Colors.orange,
-                    inactiveColor: const Color.fromARGB(155, 255, 153, 0),
-                    overlayColor: MaterialStatePropertyAll(
-                        const Color.fromARGB(255, 255, 190, 104)),
-                    labels: RangeLabels(calorieRange.start.round().toString(),
-                        calorieRange.end.round().toString()),
-                    onChanged: (RangeValues range) {
-                      ref.read(calorieRangesProvider.notifier).state = range;
-                    }))
-          ]),
-        ),
-      ),
+          margin: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color:
+                      const Color.fromARGB(255, 88, 34, 194).withOpacity(0.11))
+            ],
+          ),
+          child: FormBuilder(
+                key: _formKey,
+                child: Column(children: [
+                  searchBar(context, ref),
+                  ExpansionTile(
+                    title: const Text(
+                      "Filters",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)
+                    ),
+                    children: [filters(ref)],
+                    backgroundColor: Color.fromARGB(255, 248, 237, 220),
+                    collapsedBackgroundColor:
+                        Color.fromARGB(255, 248, 237, 220),
+                  )
+                ]),
+              )),
     ]);
   }
 
 // Recipe search bar
-
   Widget searchBar(BuildContext context, WidgetRef ref) {
-    return TextFormField(
-      //Controller stores value entered by user
-      controller: searchController,
+    return FormBuilderTextField(
+      name: "query",
       //Checks if searchbar has a value, if not: show error message
       validator: (value) {
         if (value == null || value.isEmpty) {
@@ -159,11 +155,61 @@ class Recipes extends ConsumerWidget {
               onPressed: () => onSubmit(context, ref)),
           suffixIconColor: Colors.orangeAccent,
           filled: true,
-          fillColor: Color.fromARGB(255, 248, 237, 220),
+          fillColor: Color(0xFFFAF9F6),
           //Placeholder message in search bar directing user
           hintText: "Enter Recipe Search",
           hintStyle: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
     );
+  }
+
+  Widget filters(WidgetRef ref) {
+    RangeValues calorieRange = ref.watch(calorieRangesProvider);
+    return Column(children: [
+      FormBuilderRangeSlider(
+          name: "calorieRange",
+          min: 0,
+          max: 5000,
+          initialValue: const RangeValues(0, 0),
+          divisions: 50,
+          activeColor: Colors.orange,
+          inactiveColor: const Color.fromARGB(155, 255, 153, 0),
+          labels: RangeLabels(calorieRange.start.round().toString(),
+              calorieRange.end.round().toString()),
+          decoration: const InputDecoration(labelText: "Calorie Range"),
+          onChanged: (RangeValues? range) {
+            ref.read(calorieRangesProvider.notifier).state =
+                range as RangeValues;
+          }),
+      FormBuilderFilterChip(
+        name: "diets",
+        options: const [
+          FormBuilderChipOption(value: 'vegan', child: Text("Vegan")),
+          FormBuilderChipOption(value: 'vegetarian', child: Text("Vegetarian")),
+          FormBuilderChipOption(
+              value: 'pescetarian', child: Text("Pescetarian")),
+          FormBuilderChipOption(
+              value: 'glutenFree', child: Text("Gluten Free")),
+          FormBuilderChipOption(value: 'dairyFree', child: Text("Dairy Free")),
+          FormBuilderChipOption(value: 'peanutFree', child: Text("Peanut Free"))
+        ],
+        decoration: const InputDecoration(labelText: "Dietary Restrictions"),
+        selectedColor: Colors.orange,
+        checkmarkColor: Colors.white,
+        autovalidateMode: AutovalidateMode.always,
+        validator: (value) {
+          if (value == null) {
+            return null;
+          }
+          if (value!.contains("vegan") && value!.contains("pescetarian")) {
+            return 'Recipe cannot be vegan and pesecatarian';
+          } else if (value!.contains("vegetarian") &&
+              value!.contains("pescetarian")) {
+            return 'Recipe cannot be vegetarian and pesecatarian';
+          }
+          return null;
+        },
+      )
+    ]);
   }
 
   // List of recipe results from the API request
@@ -171,7 +217,7 @@ class Recipes extends ConsumerWidget {
       //List<dynamic> recipes,
       List<Result>? recipes,
       BuildContext context,
-      WidgetRef ref) { 
+      WidgetRef ref) {
     //Expanded takes up entire container space
     if (recipes == null) {
       return const Text("INVALID API RESPONSE");
@@ -240,23 +286,21 @@ class Recipes extends ConsumerWidget {
                                   style: TextStyle(fontWeight: FontWeight.bold),
                                 )),
                             Container(
-                                        padding: EdgeInsets.all(0),
-                                        alignment: Alignment.bottomRight,
-                                        child: CircleAvatar(
-                                            radius: 20,
-                                            backgroundColor:
-                                                const Color.fromARGB(
-                                                    255, 255, 176, 58),
-                                            child: Material(
-                                                color: const Color.fromARGB(
-                                                    0, 255, 255, 255),
-                                                child: IconButton(
-                                                  onPressed: onShare(recipe, context),
-                                                  icon: const Icon(
-                                                      Icons.share),
-                                                  tooltip: "Share Recipe",
-                                                  color: Colors.white,
-                                                )))),
+                                padding: EdgeInsets.all(0),
+                                alignment: Alignment.bottomRight,
+                                child: CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor:
+                                        const Color.fromARGB(255, 255, 176, 58),
+                                    child: Material(
+                                        color: const Color.fromARGB(
+                                            0, 255, 255, 255),
+                                        child: IconButton(
+                                          onPressed: onShare(recipe, context),
+                                          icon: const Icon(Icons.share),
+                                          tooltip: "Share Recipe",
+                                          color: Colors.white,
+                                        )))),
                             Container(
                                 padding: EdgeInsets.all(0),
                                 alignment: Alignment.bottomRight,
@@ -290,12 +334,15 @@ class Recipes extends ConsumerWidget {
 
   //Form Submission Handler - Submits recipe search to API
   void onSubmit(BuildContext context, WidgetRef ref) async {
-    if (_formKey.currentState!.validate()) {
+    if (_formKey.currentState!.saveAndValidate()) {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text('Searching for Recipes')));
 
+      Map<String, dynamic> formData = _formKey.currentState!.value;
+      print(formData);
+
       //Parameter: Query - User-Inputted Search Query
-      String query = searchController.text;
+      String query = formData["query"];
       //Parameter: API Key - Used to gain access to the API
       String apiKey = "cd2573f729714ab5bba24521e30d23ec";
       //Parameter: Number - number of results to be returned from API
@@ -305,32 +352,66 @@ class Recipes extends ConsumerWidget {
       //Parameter: AddRecipeInformation - true to add recipe info such as instructions, false to not
       bool addRecipeInfo = true;
 
-      //Calorie Filter Range
       RangeValues calorieRange = ref.watch(calorieRangesProvider);
 
       //Parameter: minCalories -minimum number of calories allowed for recipe results
       int minCalories = calorieRange.start.round();
       //Parameter: maxCalories -maximum number of calories allowed for recipe results
       int maxCalories = calorieRange.end.round();
+
+      String diets = "";
+      String intolerances = "";
+
+      if (formData["diets"].contains("vegan")) {
+        diets += "vegan";
+      }
+      if (formData["diets"].contains("vegetarian")) {
+        diets += ",vegetarian";
+      }
+      if (formData["diets"].contains("pescetarian")) {
+        diets += ",pescetarian";
+      }
+      if (formData["diets"].contains("glutenFree")) {
+        diets += ",gluten free";
+      }
+      if (formData["diets"].contains("dairyFree")) {
+        intolerances += ",dairy";
+      }
+      if (formData["diets"].contains("peanutFree")) {
+        intolerances += ",peanut";
+      }
+
+      //Make sure diet string doesn't start/end with comma
+      if (diets.startsWith(',')) {
+        diets = diets.substring(1);
+      } else if (diets.endsWith(',')) {
+        diets = diets.substring(0, diets.length - 2);
+      }
+
+      //Make sure intolerance string doesn't start/end with comma
+      if (intolerances.startsWith(',')) {
+        intolerances = intolerances.substring(1);
+      } else if (intolerances.endsWith(',')) {
+        intolerances = intolerances.substring(0, intolerances.length - 2);
+      }
+
       //API Request URL with Parameters
       String url =
-          'https://api.spoonacular.com/recipes/complexSearch?apiKey=$apiKey&query=$query&number=$number&addRecipeNutrition=$addNutrition&addRecipeInformation=$addRecipeInfo&minCalories=$minCalories&maxCalories=$maxCalories';
-      //formats url
+          'https://api.spoonacular.com/recipes/complexSearch?apiKey=$apiKey';
+      url += '&query=$query';
+      url += '&number=$number';
+      url += '&addRecipeNutrition=$addNutrition';
+      url += '&addRecipeInformation=$addRecipeInfo';
+      url += '&minCalories=$minCalories';
+      url += '&maxCalories=$maxCalories';
+      url += '&diet=$diets';
+      url += '&intolerances=$intolerances';
+      print(url);
       final uri = Uri.parse(url);
-      //sends request to api
       final response = await http.get(uri);
-      //converts response from json
       final data = jsonDecode(response.body);
-      //maps API response data to RecipeModel
       RecipeModel mappedData = RecipeModel.fromJson(data);
-      //If the results is not null(or empty) we use state management
-      //to set the result equal to the mapped RecipeModel containing
-      //All of the API response data
       if (mappedData.results != null) {
-        //Update list of saved recipe Ids
-        ref.read(savedRecipeIdsProvider.notifier).state =
-            await getSavedRecipeIds();
-        //Update list of searched recipes
         ref.read(resultProvider.notifier).state = mappedData;
       } else {
         mappedData = RecipeModel();
@@ -345,7 +426,7 @@ class Recipes extends ConsumerWidget {
     if (_formKey.currentState!.validate()) {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text('Searching for Recipes')));
-      String value = searchController.text;
+      String value = _formKey.currentState!.value["query"];
       final String response =
           await rootBundle.loadString('assets/spoonacularResult.json');
       final data = await json.decode(response);
@@ -441,7 +522,7 @@ class Recipes extends ConsumerWidget {
     return recipeIDList;
   }
 
-  //list of quick-search ingredients 
+  //list of quick-search ingredients
   Widget ingredientsList(BuildContext context, WidgetRef ref) {
     List<String> ingredients = [
       "apple",
@@ -475,17 +556,16 @@ class Recipes extends ConsumerWidget {
                           width: 120,
                           padding: EdgeInsets.all(5),
                           child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll(Colors.orange)),
-                            child: Text(ingredient,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold)),
-                            //Function used to capture tap event for list items
-                            onPressed: () =>
-                                searchIngredient(context, ref, ingredient)
-                          ));
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.orange)),
+                              child: Text(ingredient,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold)),
+                              //Function used to capture tap event for list items
+                              onPressed: () =>
+                                  searchIngredient(context, ref, ingredient)));
                     }))));
   }
 
